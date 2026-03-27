ba2112e476b8e5d0b263a95fb3973da030f592de
Rajkiran Bande <bande.rajkiran@gmail.com>
2026-03-27 14:14:32 +0530

    Add app/build folder to .gitignore to remove tracking

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_app_le_bondstracker_BondsTrackerApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "app.le.bondstracker.BondsTrackerApp"
-)
-public final class _app_le_bondstracker_BondsTrackerApp {
-}
